0969add3c1cc8d66f251a04167a3a143a9d9de4d
liangweiwei0904 <2249558534@qq.com>
2021-03-23 20:59:35 +0800

    管理员管理商家和商品数据

--- pages/User/User.js
+++ pages/User/User.js
@@ -7,8 +7,43 @@ Page({
   data: {
     userinfo:{},
     //被收藏的商品数量
-    collectNums:0
+    collectNums:0,
+    tabs:[
+      {
+        id:0,
+        value:"关注商品",
+        isActive:true
+      },
+      {
+        id:1,
+        value:"关注店铺",
+        isActive:false
+      },
+      {
+        id:2,
+        value:"我的动态",
+        isActive:false
+      }
+    ],
   },
+
+   //标题的点击事件  从子组件传递过来
+   handleTabsItemChange(e){
+    //1获取被点击的标题索引
+    const {index}=e.detail;
+    //2修改源数组
+    let {tabs}=this.data;
+    tabs.forEach((v,i) =>i===index?v.isActive=true:v.isActive=false );
+    //3赋值到data中
+    this.setData({
+      tabs
+    });
+    //向后台请求数据(关注商品、店铺动态、我的动态)
+    
+
+  },
+
+
   onShow(){
     const userinfo=wx.getStorageSync("userinfo");
     const collect=wx.getStorageSync("collect")||[];
