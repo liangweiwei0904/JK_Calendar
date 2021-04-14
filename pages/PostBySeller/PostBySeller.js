659d542ab87c39dace78c0574f49fce6dbca3b52
liangweiwei0904 <2249558534@qq.com>
2021-04-14 20:26:24 +0800

    上传图片的异步操作问题

--- pages/PostBySeller/PostBySeller.js
+++ pages/PostBySeller/PostBySeller.js
@@ -1,14 +1,19 @@
+
 Page({
+
   data: {
     chooseImgs: [],
     textVal: "",
-    storeName:"",
-    goodsName:"",
-    sellTime:'2021-04-21',
-    year:"",
-    month:"",
-    day:"",
-    date: '2021-04-09'
+    storeName: "",
+    goodsName: "",
+    sellTime: '2021-04-21',
+    year: "",
+    month: "",
+    day: "",
+    date: '2021-04-09',
+    goods_img: [],
+    arr: [],
+    //temp: ["0", "cloud://lwwldx-5g8powrn59ec579e.6c77-lwwldx-5g8powrn59ec579e-1305123697/MessageImg/1616311330493.png"]
   },
   UpLoadImgs: [],
   handleChooseImg() {
@@ -45,89 +50,112 @@ Page({
       textVal: e.detail.value
     })
   },
-  handleStoreName(e){
+  handleStoreName(e) {
     this.setData({
-      storeName:e.detail.value
+      storeName: e.detail.value
     })
   },
-  handleGoodsName(e){
+  handleGoodsName(e) {
     this.setData({
-      goodsName:e.detail.value
+      goodsName: e.detail.value
     })
   },
- 
 
 
 
 
-  bindDateChange: function(e) {
+
+  bindDateChange: function (e) {
     console.log(e);
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      date:e.detail.value,
+      date: e.detail.value,
       sellTime: e.detail.value,
-      month:e.detail.value.substring(5,7),
-      day:e.detail.value.substring(8,10)
+      month: e.detail.value.substring(5, 7),
+      day: e.detail.value.substring(8, 10)
     });
-    console.log("分割之后的字符串：",this.data.month,"月",this.data.day,"日");
+    console.log("分割之后的字符串：", this.data.month, "月", this.data.day, "日");
   },
 
 
   //提交按钮的点击事件
   handleFormSubmit(e) {
-    let that=this;
+    let that = this;
+    var counter = 0;
+    console.log("this.data.chooseImgs.length" + this.data.chooseImgs.length);
     //判断用户是否上传了图片
     if (this.data.chooseImgs.length != 0) {
       this.data.chooseImgs.forEach((v, i) => {
         // 将图片上传至云存储空间
         wx.cloud.uploadFile({
           // 指定上传到的云路径
-          cloudPath: 'MessageImg/'+new Date().getTime()+'.png',//小程序官方问题，路径写死了之后上传新图片不会更换
+          cloudPath: 'MessageImg/' + new Date().getTime() + '.png',//小程序官方问题，路径写死了之后上传新图片不会更换
           // 指定要上传的文件的小程序临时文件路径
           filePath: v,
           // 成功回调
+
           success: res => {
-            //将云存储中的图片路径传给数据库
-            wx.cloud.database().collection("Goods").add({
-              data: {
-                goods_content: this.data.textVal,
-                goods_img: res.fileID,
-                store_name:this.data.storeName,
-                goods_name:this.data.goodsName,
-                sell_time:this.data.sellTime,
-                sell_year:this.data.year,
-                sell_month:this.data.month,
-                sell_day:this.data.day,
-                creater:"梁维维"
-              }
+            //将云存储中的图片路径先赋值给本地数组，再和其他数据一起传给数据库
+            that.setData({
+              ['arr[' + i + ']']: res.fileID
             })
-            .then(res1=>{
-              console.log("调用云函数成功",res1);
+            console.log("i的值" + i);
+            counter++;
+            console.log("counter" + counter);
+            if (counter == this.data.chooseImgs.length) {
+              console.log("异步执行完了");
+              //上传至数据库
+              wx.cloud.database().collection("Goods").add({
+                data: {
+                  goods_content: this.data.textVal,
+                  goods_img: this.data.arr,
+                  store_name: this.data.storeName,
+                  goods_name: this.data.goodsName,
+                  sell_time: this.data.sellTime,
+                  sell_year: this.data.year,
+                  sell_month: this.data.month,
+                  sell_day: this.data.day,
+                  creater: "梁维维"
+                }
+              })
+                .then(res1 => {
+                  console.log("test");
+                  console.log(JSON.stringify(this.data.arr));
+                  // console.log(...this.data.arr);
+                  // console.log("调用云函数成功", res1);
 
-              //商家可以传递数据给首页了
-              //可是他们两个不是同一个页面怎么办
-              //发公告之后存储到数据库
-              // 首页从数据库显示？日期如何确定？
-              //让首页进行判断
+                  //商家可以传递数据给首页了
+                  //可是他们两个不是同一个页面怎么办
+                  //发公告之后存储到数据库
+                  // 首页从数据库显示？日期如何确定？
+                  //让首页进行判断
 
-              wx.switchTab({
-                url: '/pages/Hot/Hot',
-                success: (result)=>{
-                  console.log(result,"跳转回热榜页成功");
-                },
-                fail: (result)=>{console.log(result,"跳转回热榜页失败");},
-                complete: ()=>{}
-              });
-            })
-            .catch(res1=>{
-              console.log("调用云函数失败",res1);
-            })
+                  // wx.switchTab({
+                  //   url: '/pages/Hot/Hot',
+                  //   success: (result) => {
+                  //     console.log(result, "跳转回热榜页成功");
+                  //   },
+                  //   fail: (result) => { console.log(result, "跳转回热榜页失败"); },
+                  //   complete: () => { }
+                  // });
+                })
+                .catch(res1 => {
+                  console.log("调用云函数失败", res1);
+                })
+
+            }
           },
         })
+
       })
-    }
-    else{
+
+      // console.log(this.data.temp);
 
     }
-  }  
+    else {
+
+    }
+
+
+  }
 })
